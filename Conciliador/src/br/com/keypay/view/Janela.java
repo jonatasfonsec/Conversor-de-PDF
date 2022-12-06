e7e4a18080e501890489a330ce26b8fd613e874b
jonatas <jonatas@debian-jonatas>
2022-12-06 09:15:51 -0300

    Removido a classe Painel e acrescido o JTextArea

--- Conciliador/src/br/com/keypay/view/Janela.java
+++ Conciliador/src/br/com/keypay/view/Janela.java
@@ -1,5 +1,7 @@
 package br.com.keypay.view;
 
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -9,6 +11,7 @@ import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
@@ -16,25 +19,31 @@ import javax.swing.filechooser.FileSystemView;
 import br.com.keypay.controller.ArquivoPDF;
 
 public class Janela {
+	JButton botao = new JButton();
+	Toolkit toolkit = Toolkit.getDefaultToolkit();
+	Dimension screenSize = toolkit.getScreenSize();
+	JPanel painel = new JPanel();
+	JFrame janela = new JFrame("Conversor de PDF para TXT");
+	JTextArea caixaDeTexto = new JTextArea();
 
 	@SuppressWarnings("deprecation")
 	public Janela() {
-		JButton botao = new JButton();
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		Dimension screenSize = toolkit.getScreenSize();
-		Painel painel = new Painel();
-		JFrame janela = new JFrame("Conversor de PDF para TXT");
-		JTextArea jTextArea = new JTextArea();
-		
+
+		painel.setLayout(new BorderLayout());
+		caixaDeTexto.setVisible(true);
+		caixaDeTexto.setLineWrap(true);
+		caixaDeTexto.setBounds(50, 50, 500, 500);
 		
 		
 
-		painel.add(botao);
+		painel.add(botao,BorderLayout.NORTH);
+		painel.add(caixaDeTexto,BorderLayout.SOUTH);
 
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		janela.setSize(screenSize.width, screenSize.height);
 		janela.add(painel);
 		janela.setVisible(true);
+		
 		janela.setLocationRelativeTo(null);
 		// janela.pack();
 		
@@ -66,9 +75,17 @@ public class Janela {
 		int returnValue = jfc.showOpenDialog(null);
 
 		if (returnValue == JFileChooser.APPROVE_OPTION) {
-			arquivoPDF.leArquivoPDFEArmazenaEmString(jfc.getSelectedFile().getPath());
+			
+			recebeStringDoArquivoPDF(arquivoPDF.leArquivoPDFEArmazenaEmString(jfc.getSelectedFile().getPath()));
 		}
 		
 	}
+	
+	public void recebeStringDoArquivoPDF(String textoDoArquivoPDF) {
+		//Passa a string do texto convertido para o box
+		caixaDeTexto.setText(textoDoArquivoPDF);
+	}
+	
+	
 
 }
