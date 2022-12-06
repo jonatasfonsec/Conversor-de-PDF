c40530ef4d4c8235b5dd3cef17b40f1a57af5518
jonatas <jonatas@debian-jonatas>
2022-12-06 08:26:40 -0300

    Remoção da Classe File Explorer.

--- Conciliador/src/br/com/keypay/view/Janela.java
+++ Conciliador/src/br/com/keypay/view/Janela.java
@@ -7,14 +7,16 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
-import br.com.keypay.controller.FileExplorer;
+import br.com.keypay.controller.ArquivoPDF;
 
 public class Janela {
 
-	
 	@SuppressWarnings("deprecation")
 	public Janela() {
 		JButton botao = new JButton();
@@ -22,32 +24,51 @@ public class Janela {
 		Dimension screenSize = toolkit.getScreenSize();
 		Painel painel = new Painel();
 		JFrame janela = new JFrame("Conversor de PDF para TXT");
-		JTextArea jTextArea =  new JTextArea();
-		
-		botao.setLabel("Procurar Arquivo");
-		botao.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			try {
-				new FileExplorer();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			}
-		});
+		JTextArea jTextArea = new JTextArea();
 		
 		
+		
+
 		painel.add(botao);
-		
+
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		janela.setSize(screenSize.width, screenSize.height);
 		janela.add(painel);
 		janela.setVisible(true);
 		janela.setLocationRelativeTo(null);
-		//janela.pack();
+		// janela.pack();
+		
+		
+		botao.setLabel("Procurar Arquivo");
+		botao.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					procuraArquivoPDFNoComputador();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		
 
 	}
 	
-	
-	
+	public void procuraArquivoPDFNoComputador() throws IOException {
+		
+		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		ArquivoPDF arquivoPDF = new ArquivoPDF();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF", "pdf");
+		
+		jfc.setDialogTitle("Selecione o arquivo PDF");
+		jfc.setAcceptAllFileFilterUsed(false);
+		jfc.addChoosableFileFilter(filter);
+		int returnValue = jfc.showOpenDialog(null);
+
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			arquivoPDF.leArquivoPDFEArmazenaEmString(jfc.getSelectedFile().getPath());
+		}
+		
+	}
 
 }
