2e90e2072245afb97f937c4de3689958e15e78e6
Jonatas Brandani Fonseca de Abreu <jonatas.fonsec@gmail.com>
2022-12-07 18:27:42 -0300

    Imprimindo valores.

--- Conciliador/src/br/com/keypay/model/ValorMultilateralProduto.java
+++ Conciliador/src/br/com/keypay/model/ValorMultilateralProduto.java
@@ -4,19 +4,20 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+
 public class ValorMultilateralProduto {
 
 	public static void obtemValoresParaSoma(String textoConvertido) throws IOException {
 
-		Pattern p = Pattern.compile(".*Multilateral Produto:\\s([\\d+.,]).*"); 
-
+		//Pattern p = Pattern.compile(".*Multilateral Produto:\\s([\\d+.,]).*"); 
+		Pattern p = Pattern.compile("(.*Multilateral Produto:)(\\s([\\d+.,]).*)");
 
 			Matcher m = p.matcher(textoConvertido);
 			
 			while (m.find()) {
-
-				System.out.println(m.group(0));
-
+					System.out.println(m.group(2));
+				
 			}
 
 		
