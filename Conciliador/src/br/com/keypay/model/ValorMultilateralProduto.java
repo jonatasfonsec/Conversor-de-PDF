2d1249e4a829262695202c3eef5f9d4a389ac98e
Jonatas Brandani Fonseca de Abreu <jonatas.fonsec@gmail.com>
2022-12-12 13:31:49 -0300

    Programa Concluído

--- Conciliador/src/br/com/keypay/model/ValorMultilateralProduto.java
+++ Conciliador/src/br/com/keypay/model/ValorMultilateralProduto.java
@@ -1,35 +1,42 @@
 package br.com.keypay.model;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.JOptionPane;
 
 public class ValorMultilateralProduto {
 
-	static List<String> listaValoresEmString = new ArrayList<String>();
+	public static void obtemValoresParaSoma(String textoConvertido) {
 
-	@SuppressWarnings("unlikely-arg-type")
-	public static void obtemValoresParaSoma(String textoConvertido){
+		DecimalFormat df = new DecimalFormat("###,###,###.00");
+		String regexp = "multilateral\\s+produto\\s*:\\s*([\\d.,]+)\\s*";
+		Pattern MULTILATERAL_PRODUTO = Pattern.compile(regexp, Pattern.CASE_INSENSITIVE);
+		Scanner scanner = null;
+		List<Double> valores = new ArrayList<Double>();
+		double soma = 0;
 
-		Pattern p = Pattern.compile("(Multilateral Produto:\\s)([\\d.;,]*)");
-		Matcher m = p.matcher(textoConvertido);
-
-		while (m.find()) {
-			listaValoresEmString.add(m.group(2));
+		try {
+			scanner = new Scanner(textoConvertido);
+			while (scanner.hasNextLine()) {
+				String linha = scanner.nextLine();
+				Matcher matcher = MULTILATERAL_PRODUTO.matcher(linha);
+				if (!matcher.find())
+					continue;
+				String valorString = matcher.group(1);
+				Double valorDouble = Double.parseDouble(valorString.trim().replaceAll("\\.", "").replace(",", "."));
+				valores.add(valorDouble);
+				soma += valorDouble;
+			}
+			String dx = ("Total de Valores Multilateral Produto: R$ "+df.format(soma));
+			JOptionPane.showMessageDialog (null, dx);
+		} finally {
+			scanner.close();
 		}
-
-		
-		for(int i = 0;i>=listaValoresEmString.size();i++) {
-			String str = listaValoresEmString.get(i).replace(".","").replace(",","");
-			listaValoresEmString.set(listaValoresEmString.indexOf(i),str);
-		}
-		System.out.println(listaValoresEmString);
-		
-
-		
-	}		
-
+	}
 
 }
