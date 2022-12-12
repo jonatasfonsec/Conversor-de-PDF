dfdd5cf87c87ac85212fa320f56da7b9d04bda3c
Jonatas Brandani Fonseca de Abreu <jonatas.fonsec@gmail.com>
2022-12-12 14:53:01 -0300

    Refatorado.

--- Conciliador/src/br/com/keypay/model/ValorMultilateralProduto.java
+++ Conciliador/src/br/com/keypay/model/ValorMultilateralProduto.java
@@ -11,14 +11,14 @@ import javax.swing.JOptionPane;
 
 public class ValorMultilateralProduto {
 
+	static DecimalFormat df = new DecimalFormat("###,###,###.00");
+	static String regexp = "multilateral\\s+produto\\s*:\\s*([\\d.,]+)\\s*";
+	static Pattern MULTILATERAL_PRODUTO = Pattern.compile(regexp, Pattern.CASE_INSENSITIVE);
+	static Scanner scanner = null;
+	static double soma = 0;
+
 	public static void obtemValoresParaSoma(String textoConvertido) {
 
-		DecimalFormat df = new DecimalFormat("###,###,###.00");
-		String regexp = "multilateral\\s+produto\\s*:\\s*([\\d.,]+)\\s*";
-		Pattern MULTILATERAL_PRODUTO = Pattern.compile(regexp, Pattern.CASE_INSENSITIVE);
-		Scanner scanner = null;
-		List<Double> valores = new ArrayList<Double>();
-		double soma = 0;
 
 		try {
 			scanner = new Scanner(textoConvertido);
@@ -29,7 +29,6 @@ public class ValorMultilateralProduto {
 					continue;
 				String valorString = matcher.group(1);
 				Double valorDouble = Double.parseDouble(valorString.trim().replaceAll("\\.", "").replace(",", "."));
-				valores.add(valorDouble);
 				soma += valorDouble;
 			}
 			String dx = ("Total de Valores Multilateral Produto: R$ "+df.format(soma));
